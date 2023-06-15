fa43cb7ab1ffc736ecf246a0ee24ed686c906772
aravindkv27 <aravind27032002@gmail.com>
2023-06-15 22:08:07 +0530

    spotify clone home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SongCardComponent } from './components/song-card/song-card.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistComponent,
+    SearchComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    SongCardComponent,
+    TopNavComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
